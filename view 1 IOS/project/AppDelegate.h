684dbfab3c298a22950eac12326d84f98c011400
psyko17200 <skydesigncorporation@gmail.com>
2019-03-29 11:30:17 +0100

    vue 1 IOS récupération de trame

---
+++ view 1 IOS/project/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  project
+//
+//  Created by Guillaume Paillard on 25/03/2019.
+//  Copyright © 2019 Guillaume Paillard. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ view 1 IOS/project/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  project
+//
+//  Created by Guillaume Paillard on 25/03/2019.
+//  Copyright © 2019 Guillaume Paillard. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
